e34cead4a1c4eb7c6f960b679f1d0576b9c356eb
Odiend1 <diendaosric@gmail.com>
2024-11-27 16:03:11 -0600

    Made nav bar blur backdrop when scrolled down

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var titleDiv = document.getElementById("title-div");
 var title = document.getElementById("title");
 var navBar = document.getElementById("nav-bar");
+var navLinks = document.getElementsByClassName("nav-link");
+var navLinkContainer = document.getElementById("nav-link-container");
 var titleDivHeight = window.innerHeight - navBar.clientHeight;
 
 function resizeTitleDiv(){
@@ -16,3 +18,22 @@ function resizeTitleDivFull(){
 resizeTitleDivFull();
 window.addEventListener("resize", resizeTitleDivFull);
 
+var lightNavLinkColor = true;
+addEventListener("scroll", function(){
+    if(window.scrollY > titleDiv.clientHeight-30 && lightNavLinkColor){
+        lightNavLinkColor = false;
+        for(let i = 0; i < navLinks.length; i++){
+            navLinks[i].style.color = "#000";
+        }
+        navBar.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+        navBar.style.backdropFilter = "blur(5px)";
+    }
+    else if(window.scrollY < titleDiv.clientHeight-30 && !lightNavLinkColor){
+        lightNavLinkColor = true;
+        for(let i = 0; i < navLinks.length; i++){
+            navLinks[i].style.color = "#fff";
+        }
+        navBar.style.backgroundColor = "transparent";
+        navBar.style.backdropFilter = "none";
+    }
+})
