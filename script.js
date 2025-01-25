cdc97b5c8a2f7f4eb9ec3b2ec429a12d9866292d
Odiend1 <diendaosric@gmail.com>
2025-01-25 17:03:11 -0600

    Redesigned home page

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-var titleDiv = document.getElementById("title-div");
-var title = document.getElementById("title");
-var navBar = document.getElementById("nav-bar");
-var navLinks = document.getElementsByClassName("nav-link");
-var navLinkContainer = document.getElementById("nav-link-container");
-var titleDivHeight = window.innerHeight - navBar.clientHeight;
+// var titleDiv = document.getElementById("title-div");
+// var title = document.getElementById("title");
+// var navBar = document.getElementById("nav-bar");
+// var navLinks = document.getElementsByClassName("nav-link");
+// var navLinkContainer = document.getElementById("nav-link-container");
+// var titleDivHeight = window.innerHeight - navBar.clientHeight;
 
-function resizeTitleDiv(){
-    titleDiv.style.height = window.innerHeight - navBar.clientHeight + "px";
-    titleDiv.style.marginTop = navBar.clientHeight + "px";
-}
+// function resizeTitleDiv(){
+//     titleDiv.style.height = window.innerHeight - navBar.clientHeight + "px";
+//     titleDiv.style.marginTop = navBar.clientHeight + "px";
+// }
 
-var lightNavLinkColor = true;
-addEventListener("scroll", function(){
-    if(window.scrollY > titleDiv.clientHeight-30 && lightNavLinkColor){
-        lightNavLinkColor = false;
-        for(let i = 0; i < navLinks.length; i++){
-            navLinks[i].style.color = "#000";
-        }
-        navBar.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
-        navBar.style.backdropFilter = "blur(5px)";
-        navBar.setAttribute("style","-webkit-filter:blur(5px)");
-    }
-    else if(window.scrollY < titleDiv.clientHeight-30 && !lightNavLinkColor){
-        lightNavLinkColor = true;
-        for(let i = 0; i < navLinks.length; i++){
-            navLinks[i].style.color = "#fff";
-        }
-        navBar.style.backgroundColor = "transparent";
-        navBar.style.backdropFilter = "none";
-        navBar.setAttribute("style","-webkit-filter:none");
-    }
-})
+// var lightNavLinkColor = true;
+// addEventListener("scroll", function(){
+//     if(window.scrollY > titleDiv.clientHeight-30 && lightNavLinkColor){
+//         lightNavLinkColor = false;
+//         for(let i = 0; i < navLinks.length; i++){
+//             navLinks[i].style.color = "#000";
+//         }
+//         navBar.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+//         navBar.style.backdropFilter = "blur(5px)";
+//         navBar.setAttribute("style","-webkit-filter:blur(5px)");
+//     }
+//     else if(window.scrollY < titleDiv.clientHeight-30 && !lightNavLinkColor){
+//         lightNavLinkColor = true;
+//         for(let i = 0; i < navLinks.length; i++){
+//             navLinks[i].style.color = "#fff";
+//         }
+//         navBar.style.backgroundColor = "transparent";
+//         navBar.style.backdropFilter = "none";
+//         navBar.setAttribute("style","-webkit-filter:none");
+//     }
+// })
 
-var backgroundVideo = document.getElementById("background-video");
-var slideshowVideos = ["sandwich", "salad", "pizza", "pasta"];
-var slideshowIndex = 0;
-setInterval(function(){
-    backgroundVideo.src = "./assets/videos/title-slideshow/gp-" + slideshowVideos[slideshowIndex] + ".mp4";
-    slideshowIndex++;
-    if(slideshowIndex > 3) slideshowIndex = 0;
-}, 5500)
+// var backgroundVideo = document.getElementById("background-video");
+// var slideshowVideos = ["sandwich", "salad", "pizza", "pasta"];
+// var slideshowIndex = 0;
+// setInterval(function(){
+//     backgroundVideo.src = "./assets/videos/title-slideshow/gp-" + slideshowVideos[slideshowIndex] + ".mp4";
+//     slideshowIndex++;
+//     if(slideshowIndex > 3) slideshowIndex = 0;
+// }, 5500)
