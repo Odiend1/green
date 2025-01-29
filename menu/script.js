09b3de81ea61f219551f43761f76e59c7b150bc3
Odiend1 <diendaosric@gmail.com>
2025-01-28 20:30:29 -0600

    Fixed glitching menu images

--- menu/script.js
+++ menu/script.js
@@ -56,6 +56,6 @@ for(let i = 0; i < menuItems.categories.length; i++){
   var category = menuItems.categories[i];
   var categoryContainer = document.getElementById(category.toLowerCase().replaceAll(" ", "-") + "-container");
   for(let j = 0; j < menuItems.items[category].length; j++){
-    try{ categoryContainer.innerHTML += `<div class="menu-item"><img src="/assets/images/menu-items/${menuItems.items[category][j].name.toLowerCase().replaceAll(" ", "-")}.png" class="menu-item-img" onerror="this.src='/assets/images/item-not-found.png'"><h3 class="menu-item-title">${menuItems.items[category][j].name}</h3><p class="menu-item-desc">$${menuItems.items[category][j].price}</p><button class="menu-item-add" onclick="window.location.href = './item/?item=${getItemId(menuItems.items[category][j].name)}'">ADD</button></div>` } catch(e){}
+    categoryContainer.innerHTML += `<div class="menu-item"><img src="/assets/images/menu-items/${menuItems.items[category][j].name.toLowerCase().replaceAll(" ", "-")}.png" class="menu-item-img" onerror="this.src='/assets/images/item-not-found.png'"><h3 class="menu-item-title">${menuItems.items[category][j].name}</h3><p class="menu-item-desc">$${menuItems.items[category][j].price}</p><button class="menu-item-add" onclick="window.location.href = './item/?item=${getItemId(menuItems.items[category][j].name)}'">ADD</button></div>`;
   }
 }
